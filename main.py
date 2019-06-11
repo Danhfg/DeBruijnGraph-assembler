d0d2b84a13cd8dc46956b684426f81b139f938b6
Daniel Henrique Ferreira Gomes <danielhenriquefg@gmail.com>
2019-06-11 09:34:44 -0300

    adicionando teste longo

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import de_bruijn as dbg
 filename = 'short.fasta'
 reads = dbg.read_reads(filename)
 
-test = ['aabbbba', 'ybb']
-g = dbg.deBruijn(test, 2)
-dbg.removeTips(g)
-dbg.print_graph(g)
+#test = ['aabbbba', 'ybb']
+g = dbg.deBruijn(reads, 20)
+#dbg.removeTips(g)
+#dbg.print_graph(g)
 
-#contig = dbg.eulerian_path_search(g)
+contig = dbg.eulerian_path_search(g)
 
-#print(contig)
+print(contig)
