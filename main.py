caef717cbab0d96e24680eaa0406f3cc56aec827
Daniel Henrique Ferreira Gomes <danielhenriquefg@gmail.com>
2019-06-12 18:58:00 -0300

    Adicionando saida de arquivo

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 import de_bruijn as dbg
 import sys
+from Bio import SeqIO
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
 
-filename = sys.argv[1]#'short.fasta'
-reads = dbg.read_reads(filename)
+@profile
+def main():
+    filename = sys.argv[1]#'short.fasta'
+    reads = dbg.read_reads(filename)
 
-#test = ['aabbbba', 'ybb']
-g = dbg.deBruijn(reads, 20)
-dbg.removeTips(g)
-#dbg.print_graph(g)
+    #test = ['aabbbba', 'ybb']
+    g = dbg.deBruijn(reads, 20)
+    dbg.removeTips(g)
+    #dbg.print_graph(g)
 
-contig = dbg.eulerian_path_search(g)
+    contig = dbg.eulerian_path_search(g)
 
-print(contig)
+    SeqIO.write(SeqRecord(Seq(contig), id="UNKNOWN", description='sequence assembled from ' +sys.argv[1] ), sys.argv[2], "fasta")
+
+if __name__ == '__main__':
+    if len(sys.argv) > 2:
+        main()
