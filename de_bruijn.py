98014b8a3d810fa0280d136c1d7768c96edf39b4
Henrique David de Medeiros <henriquemed101@gmail.com>
2019-06-12 23:51:12 -0300

    Algoritmos

--- de_bruijn.py
+++ de_bruijn.py
@@ -79,18 +79,18 @@ def print_graph(graph):
     Vert = graph[0]
     Arest = graph[1]
 
-    print(Arest.keys())
 
     for k in list(Vert.keys()):
         print("nome: ", Vert[k].nome, ". entrada: ", Vert[k].entrada, ". saida: ", Vert[k].saida)
         print("Arestas para: ")
-        for e in Arest[k]:
-            print(e.nome)
+        #for e in Arest[k]:
+            #print(e.nome)
 
 # Realiza a remoção das tips.
 # Observar se possui algum vértice ( diferente do inicio ) que não possui conexão de entrada
 # com nenhum outro vertice. De forma que ele seja uma nova fonte, dai remove ele.
-# Como a remoção é de suposto segundo fonte, então não há problemas.
+# Como a remoção é do suposto segundo fonte, então não há problemas.
+
 def removeTips(graph):
     Vert = graph[0]     # obtemos os vertices
     Arest = graph[1]    # obtemos as arestas do grafo#
@@ -99,16 +99,20 @@ def removeTips(graph):
     rem_vert = []
     rem_ares = []
     
+    
     for index, k in enumerate(list(Vert.keys())):             # percorre cada vertice
         if k != lista[0] and Vert[k].entrada == 0:  # verifica se não é o fonte e se possui grau de entrada 0
             #Verificar os vértices que possuem alguma conexão para dar baixa
             
-            rem_ares.append(k)    # remove arestas referente ao vértice
+            #adiciona os vértices a ser removidos
+            rem_ares.append(k)
             rem_vert.append(k)
+
+    qnt_ares = 0
     
     for i in rem_ares:
         for e in Arest[i]:
-            print(e.nome)
+            qnt_ares += 1
             for k in list(Vert.keys()):     # percorre cada vertice
                 if e.nome == k :            # verfica se o vetor atual vai para o vertice que se deseja excluir
                     Vert[k].entrada -= 1          # diminui o grau de entrada do vertice atual (considerar exclusão).
--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import de_bruijn as dbg
+import time
+
+start_time = time.time()
 
 filename = 'short.fasta'
 reads = dbg.read_reads(filename)
 
-test = ['aabbbba', 'ybb']
-g = dbg.deBruijn(test, 2)
+#test = ['abcefg', 'bwqf']
+g = dbg.deBruijn(reads, 2)
 dbg.removeTips(g)
 dbg.print_graph(g)
 
-#contig = dbg.eulerian_path_search(g)
+contig = dbg.eulerian_path_search(g)
 
-#print(contig)
+print(contig)
+
+print('--- %s seconds ---' % (time.time() - start_time))
